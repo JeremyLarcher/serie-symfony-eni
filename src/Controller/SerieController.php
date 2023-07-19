e4cba86ff8f98451c4371fe5b52d4d6d23f5a7ea
JeremyLarcher <larcherjeremy@hotmail.fr>
2023-07-19 11:19:18 +0200

    Version 1 projet final

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Form\SerieType;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -15,31 +21,106 @@ class SerieController extends AbstractController
     /**
      * @Route("", name="list")
      */
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
-        //todo : aller chercher les series en base de donnée
-        return $this->render('serie/list.html.twig', [
+        $series = $serieRepository->findBestSeries();
 
+        return $this->render('serie/list.html.twig', [
+            "series" => $series,
         ]);
     }
 
     /**
      * @Route("/details/{id}", name="details")
      */
-    public function details(int $id): Response
+    public function details(int $id, SerieRepository $serieRepository): Response
     {
-        //todo : aller chercher la series en base de donnée
-        return $this->render('serie/details.html.twig');
+        $serie = $serieRepository->find($id);
+        if (!$serie){
+            throw $this->createNotFoundException('oh no!!!');
+        }
+        return $this->render('serie/details.html.twig', [
+            "serie" => $serie,
+        ]);
 
     }
 
     /**
-     * @Route("/create}", name="create")
+     * @Route("/create", name="create")
      */
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
-        //todo : aller chercher la series en base de donnée
+        $serie = new Serie();
+        $serie->setDateCreated(new \DateTime());
+        $serieForm = $this->createForm(SerieType::class, $serie);
+
+        $serieForm->handleRequest($request);
+
+        if ($serieForm->isSubmitted() && $serieForm->isValid()) {
+
+
+            $entityManager->persist($serie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Serie added! Good job');
+            return $this->redirectToRoute('serie_details', ['id' => $serie->getId()]);
+        }
+
+        return $this->render('serie/create.html.twig', [
+            'serieForm' => $serieForm->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/demo", name="em-demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        //creer une instance de l'entité
+        $serie = new Serie();
+
+        //hydrater toutes les propriétés
+        $serie->setName('pif');
+        $serie->setBackdrop('dafsd');
+        $serie->setPoster('dafsdf');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("- 1 year"));
+        $serie->setLastAirDate(new \DateTime("- 6 month"));
+        $serie->setGenres('drama');
+        $serie->setOverview('bla bla bla');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus('canceled');
+        $serie->setTmdbId(329432);
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        //remove pour supprimer de la bdd
+        //$entityManager->remove($serie);
+
+        $serie->setGenres('comedy');
+
+        $entityManager->flush();
+
         return $this->render('serie/create.html.twig');
 
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete($id, SerieRepository $serieRepository, EntityManagerInterface $entityManager)
+    {
+        $serie = $serieRepository->find($id);
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main_home');
     }
 }
